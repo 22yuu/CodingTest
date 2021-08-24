899c64243da3dcd3a146d13fb6d6c9889ccee63f
yuu <22yuu@naver.com>
2021-08-24 23:36:54 +0900

    2021/08/24 프로그래머스 위클리 챌린지 4주차 직업군 추천하기

---
+++ Programmers/src/weeklyChallenge/week4.java
@@ -0,0 +1,14 @@
+package weeklyChallenge;
+/*
+ * 프로그래머스 weekly challenge 4주차
+ * 레벨 2 직업군 추천하기
+ * https://programmers.co.kr/learn/courses/30/lessons/84325
+ * 
+ */
+public class week4 {
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
