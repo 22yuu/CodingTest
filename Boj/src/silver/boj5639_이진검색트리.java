8be183304e56eac1aa04be58b01b21e98bc5ddc3
yuu <22yuu@naver.com>
2021-08-29 21:46:25 +0900

    2021/08/29 백준 5639 이진 검색 트리

---
+++ Boj/src/silver/boj5639_이진검색트리.java
@@ -0,0 +1,12 @@
+package silver;
+
+import java.util.*;
+import java.io.*;
+
+public class boj5639_이진검색트리 {
+
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
