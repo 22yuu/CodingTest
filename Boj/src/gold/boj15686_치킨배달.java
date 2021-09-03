345a142ced8ad926aab4bda28488281e8facae28
yuu <22yuu@naver.com>
2021-09-04 00:51:42 +0900

    2021/09/04 백준 15686 치킨 배달

---
+++ Boj/src/gold/boj15686_치킨배달.java
@@ -0,0 +1,32 @@
+package gold;
+
+import java.util.*;
+import java.io.*;
+
+public class boj15686_치킨배달 {
+	
+	public static int N, M;
+	public static int map[][];
+	public static boolean visited[][];
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		
+	}
+}
