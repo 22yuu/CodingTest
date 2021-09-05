0736363f7e89bee2f029bad7b4b8a35a1f3994f5
yuu <22yuu@naver.com>
2021-09-05 23:50:11 +0900

    commit

--- Boj/src/gold/boj15686_치킨배달.java
+++ Boj/src/gold/boj15686_치킨배달.java
@@ -3,12 +3,32 @@ package gold;
 import java.util.*;
 import java.io.*;
 
+
+/*
+ * 0은 빈칸, 1은 집, 2는 치킨집
+ * 
+ */
 public class boj15686_치킨배달 {
 	
 	public static int N, M;
 	public static int map[][];
+	public static int dx[] = {-1, 1, 0,  0};
+	public static int dy[] = { 0, 0, 1, -1};
 	public static boolean visited[][];
+	public static int[][] dist;
 	
+	public static void dfs(int x, int y) {
+		
+		
+		for(int d = 0; d < 4; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+			
+			
+		}
+	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,16 +37,34 @@ public class boj15686_치킨배달 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		map = new int[N][N];
-		visited = new boolean[N][N];
+		int nHomes = 0;
+		int nChickens = 0;
 		
-		for(int i = 0; i < N; i++) {
+		map = new int[N+1][N+1];
+		visited = new boolean[N+1][N+1];
+		
+		for(int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 1; j <= N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				map[i][j] = num;
+				
+				if(num == 1) {
+					nHomes++;
+				} else if( num == 2) {
+					nChickens++;
+				}
 			}
 		}
 		
+		dist = new int[nHomes][nChickens];
 		
+		
+		
+		
+	}
+	
+	public static int getDistance() {
+		return 0;
 	}
 }
