30b8228466e58b11de41181a13eaed4cc2d0aa09
yuu <22yuu@naver.com>
2021-09-20 23:34:56 +0900

    2021/09/20 프로그래머스 뉴스클러스터링_

---
+++ Programmers/src/lv2/뉴스클러스터링.java
@@ -0,0 +1,15 @@
+package lv2;
+
+import java.util.*;
+import java.io.*;
+
+public class 뉴스클러스터링 {
+
+	public static void main(String[] args) {
+		String str1 = "france";
+		
+		str1 = str1.toUpperCase();
+		
+		System.out.println(str1);
+	}
+}
