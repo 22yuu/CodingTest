a22c302ae129c2f45896acd8a30a3d992180a0f4
yuu <22yuu@naver.com>
2021-09-22 23:43:44 +0900

    commit

--- Programmers/src/lv2/뉴스클러스터링.java
+++ Programmers/src/lv2/뉴스클러스터링.java
@@ -4,15 +4,28 @@ import java.util.*;
 import java.io.*;
 
 public class 뉴스클러스터링 {
-
+	
+	public static String pattern = "[^a-zA-Z]";
+	
 	public static void main(String[] args) {
 		String str1 = "france";
+		String str2 = "E=M*C^2";
 		
+		str1 = str1.replace(pattern, "");
+        str2 = str2.replaceAll(pattern, "");
+        
+        System.out.println(str2);
+        
 		str1 = str1.toUpperCase();
 		
-		System.out.println('A' - 65);
-		System.out.println('*' - 65);
-		System.out.println('+' - 65);
-		System.out.println(str1);
+        for(int i = 0; i < str1.length() - 1; i++) {
+            String temp = str1.substring(i, i+2);
+            System.out.println(temp);
+        }
+		
+//		System.out.println('A' - 65);
+//		System.out.println('*' - 65);
+//		System.out.println('+' - 65);
+//		System.out.println(str1);
 	}
 }
