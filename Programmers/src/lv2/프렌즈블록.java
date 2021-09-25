1893d413b5f617424a66cd5ba25e684d00783348
yuu <22yuu@naver.com>
2021-09-25 23:15:08 +0900

    2021/09/25 프로그래머스 프렌즈블록 실패

--- Programmers/src/lv2/프렌즈블록.java
+++ Programmers/src/lv2/프렌즈블록.java
@@ -9,6 +9,7 @@ public class 프렌즈블록 {
     public static int dy[] = {0, 1, 1}; // 오른쪽, 아래, 오른쪽 대각
     public static int N,M;
     public static char[][] map;
+    public static boolean[][] visited;
     public static ArrayList<Point> blocks = new ArrayList<>();
     
     public static class Point {
@@ -41,14 +42,14 @@ public class 프렌즈블록 {
                 
                 if(nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
                 
-                if(ch == map[nx][ny]) {
+                if(ch == map[nx][ny] && ch != '0') {
                     count++;
-                    System.out.println("count : " + count);
-                    System.out.println("map[" + nx + "][" + ny + "] :" + map[nx][ny]);
+                    //System.out.println("count : " + count);
+                    //System.out.println("map[" + nx + "][" + ny + "] :" + map[nx][ny]);
                     
                     if(count == 4) {
                         blocks.add(new Point(np.x, np.y));
-                        System.out.println(np.x + ", " + np.y);
+                        //System.out.println(np.x + ", " + np.y);
                     }
                 }
                 
@@ -68,15 +69,48 @@ public class 프렌즈블록 {
              map[i] = board[i].toCharArray();
          }
          
+//         while(true) {
+//             visited = new boolean[M][N];
+//             bfs(0,0);
+//             
+//             if(blocks.size() > 0) {
+//                 answer += getScore();
+//             } else {
+//                 return answer;
+//             }
+//             blocks.clear();
+//             reBuild();
+//             print();
+//             
+//         }
+         
+         visited = new boolean[M][N];
          bfs(0,0);
+         
          if(blocks.size() > 0) {
              answer += getScore();
          }
          blocks.clear();
+         System.out.println("blocks size " + blocks.size());
+         reBuild();
          print();
-
+         
+         visited = new boolean[M][N];
+         bfs(0,0);
+         
+         if(blocks.size() > 0) {
+             answer += getScore();
+         }
+         System.out.println("blocks size " + blocks.size());
+         if(blocks.size() > 0) {
+        	 Point np = blocks.get(0);
+        	 
+        	 System.out.println(np.x + ", " + np.y);
+         }
+         blocks.clear();
+         reBuild();
+         print();
          return answer;
-       
     }
     
     public static int getScore() {
@@ -86,9 +120,11 @@ public class 프렌즈블록 {
         
         for(int i = 0; i < size; i++) {
             Point np = blocks.get(i);
-            
-            map[np.x][np.y] = '0';
-            score++;
+
+            if(map[np.x][np.y] != '0') {
+                map[np.x][np.y] = '0';
+                score++;
+            }
             
             for(int d = 0; d < 3; d++) {
                 int nx = np.x + dx[d];
@@ -106,6 +142,30 @@ public class 프렌즈블록 {
         return score;
     }
     
+    public static void reBuild() {
+        // 빈 공간을 채우는 함수
+        
+        while(true) {
+        	int count = 0;
+        	for(int j = 0; j < N; j++) {
+                for(int i = 0; i < M-1; i++) {
+                	
+                	if(map[i][j] == '0') continue;
+                	
+                	//System.out.println(map[i][j]);
+                	
+                    if(map[i+1][j] == '0') {
+                        map[i+1][j] = map[i][j];
+                        map[i][j] = '0';
+                        count++;
+                    }
+                }
+            }
+        	
+        	if(count == 0) return;
+        }
+    }
+    
     public static void print() {
         for(int i = 0; i < M; i++) {    
             for(int j = 0; j < N; j++) {    
@@ -117,6 +177,9 @@ public class 프렌즈블록 {
     
     public static void main(String[] args) {
     	String[] board = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
+    	String[] board2 = {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
+    	
     	System.out.println(solution(4, 5, board));
+    	System.out.println(solution(6, 6, board2));
 	}
 }
