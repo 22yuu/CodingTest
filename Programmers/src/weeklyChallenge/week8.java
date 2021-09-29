c9beacdcd9a7c5c8ac26cde2d3a241fe2bdf17b7
yuu <22yuu@naver.com>
2021-09-29 23:49:59 +0900

    2021/09/29 위클리 8주차 최소직사각형

--- Programmers/src/weeklyChallenge/week8.java
+++ Programmers/src/weeklyChallenge/week8.java
@@ -2,4 +2,20 @@ package weeklyChallenge;
 
 public class week8 {
 
+    public static int solution(int[][] sizes) {
+        int answer = 0;
+        
+        // 첫 for문에 회전 안시키고 비교
+        
+        
+        
+        // 회전 시키고 비교
+        
+        
+        return answer;
+    }
+    
+	public static void main(String[] args) {
+		
+	}
 }
