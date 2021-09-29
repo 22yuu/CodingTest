6441aed55f7d1b42bc3b3a7d7d2326edeee90a7b
yuu <22yuu@naver.com>
2021-09-30 01:02:17 +0900

    2021/09/30 프로그래머스 위클리 챌린지 8주차 최소직사각형

--- Programmers/src/weeklyChallenge/week8.java
+++ Programmers/src/weeklyChallenge/week8.java
@@ -2,20 +2,38 @@ package weeklyChallenge;
 
 public class week8 {
 
-    public static int solution(int[][] sizes) {
-        int answer = 0;
-        
-        // 첫 for문에 회전 안시키고 비교
-        
-        
-        
-        // 회전 시키고 비교
-        
-        
-        return answer;
-    }
+	 public static int solution(int[][] sizes) {
+	        int answer = 0;
+	        int length = sizes.length;
+	        int widthMax = 0;
+	        int heightMax = 0;
+	        
+	        for(int i = 0; i < length; i++) {
+	            if(sizes[i][0] < sizes[i][1]) {
+	                int temp = sizes[i][0];
+	                sizes[i][0] = sizes[i][1];
+	                sizes[i][1] = temp;
+	            }
+	        }
+	        
+	        for(int i = 0; i < length; i++) {
+	            widthMax = Math.max(widthMax, sizes[i][0]);
+	            heightMax = Math.max(heightMax, sizes[i][1]);
+	        }
+	        
+	        
+	        answer = widthMax * heightMax;
+	        
+	        return answer;
+	    }
     
 	public static void main(String[] args) {
+		int[][] arr1 = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+		int[][] arr2 = {{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}};
+		int[][] arr3 = {{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}};
 		
+		System.out.println(solution(arr1));
+		System.out.println(solution(arr2));
+		System.out.println(solution(arr3));
 	}
 }
