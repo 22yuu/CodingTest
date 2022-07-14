9173d1c2c80f89326c37f5f41cd0ed85df371c5d
yuu <22yuu@naver.com>
2022-07-14 22:35:25 +0900

    백준 농구경기

--- Boj/src/bronze/boj1159_농구경기.java
+++ Boj/src/bronze/boj1159_농구경기.java
@@ -5,8 +5,45 @@ import java.io.*;
 
 public class boj1159_³ó±¸°æ±â {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static int N;
+	private static HashMap<Character, Integer> hm;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		N = Integer.parseInt(br.readLine());
+		hm = new HashMap<>();
+		String answer = "";
+		
+		for(int i = 0; i < N; i++) {
+			String str = br.readLine();
+			
+			char ch = str.charAt(0);
+			
+			if(hm.get(ch) != null ) {
+				hm.put(ch, hm.get(ch) + 1);
+				continue;
+			}
+			
+			hm.put(ch, 1);
+		}
+		
+		
+		for(char ch : hm.keySet()) {
+			if(hm.get(ch) >= 5) answer += ch;
+			System.out.println(ch + " : " +  hm.get(ch));
+		}
+		
+		char[] StringToChar = answer.toCharArray();
+		
+		Arrays.sort(StringToChar);
+		
+		answer = new String(StringToChar);
+		
+		if(!answer.equals("")) System.out.println(answer);
+		else System.out.println("PREDAJA");
+		
+		
 
 	}
 
---
+++ Boj/src/bronze/boj11655_ROT13.java
@@ -0,0 +1,22 @@
+package bronze;
+
+import java.util.*;
+import java.io.*;
+
+public class boj11655_ROT13 {
+	
+	public static String Convert(String s) {
+		
+		return "";
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] strArr = br.readLine().split(" ");
+		
+		
+		
+
+	}
+
+}
