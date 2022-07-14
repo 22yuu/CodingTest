3d00879cb833c43131ca2e119dd7598b2717d63e
yuu <22yuu@naver.com>
2022-07-14 21:59:56 +0900

    백준 트럭주차

--- Boj/src/bronze/boj2979_트럭주차.java
+++ Boj/src/bronze/boj2979_트럭주차.java
@@ -5,78 +5,36 @@ import java.io.*;
 
 public class boj2979_트럭주차 {
 	
-	private static int[][] trucks;
-	private static int[] fees;
-	private static List<Integer> in;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		trucks = new int[3][2];
-		in = new ArrayList<Integer>();
-		
-		int feeA = Integer.parseInt(st.nextToken());
-		int feeB = Integer.parseInt(st.nextToken());
-		int feeC = Integer.parseInt(st.nextToken());
-		
-		fees = new int[] {feeA, feeB, feeC};
-		
-		int totalCost = 0;
-		
-		for(int i = 0 ; i < 3; i++) {
-			st = new StringTokenizer(br.readLine());
-			trucks[i][0] = Integer.parseInt(st.nextToken());
-			trucks[i][1] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(trucks, new Comparator<int[]>() {
-
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[0] == o2[0]) {
-					return o1[1] - o2[1];
-				} else {
-					return o1[0] - o2[0];
-				}
-			}
-		});
-		
-		int firstTime = trucks[0][0];
-		in.add(firstTime);
-		totalCost = feeA;
-		
-		for(int i = firstTime+1 ; i <= 100; i++) {
-			
-			
-			
-			for(int j = 0; j < 3; j++) {
-				
-				if(i == trucks[j][0]) {
-					// 주차장에 들어온 시간
-					in.add(i);
-				}
-				
-				if(i == trucks[j][1]) {
-					// 트럭이 나간 시간
-//					totalCost += in.size() * fees[in.size() - 1];
-					in.remove(Integer.valueOf(trucks[j][0]));
-					continue;
-				}
-			}
-			
-			if(in.size() < 1) break;
-			
-			System.out.print(i+ ":" + in.size() +  "x" + fees[in.size() - 1] + " ");
-			totalCost += in.size() * fees[in.size() - 1];
-			System.out.println(totalCost);
-		}
-		
-//		for(int[] n : trucks) System.out.println(n[0] + " " + n[1]);
-		System.out.println("list size : " + in.size());
-		System.out.println(totalCost);
-		
-		
-		
-	}
-
+	 	private static int[] arr;
+	    private static int[] fees;
+	    
+	    public static void main(String[] args) throws Exception {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st = new StringTokenizer(br.readLine());
+	        
+	        int totalFee = 0;
+	        int feeA = Integer.parseInt(st.nextToken());
+	        int feeB = Integer.parseInt(st.nextToken());
+	        int feeC = Integer.parseInt(st.nextToken());
+	        
+	        fees = new int[] {feeA, feeB, feeC};
+	        arr = new int[101];
+	        
+	        
+	        for(int i = 0; i < 3; i++) {
+	            st = new StringTokenizer(br.readLine());
+	            int in = Integer.parseInt(st.nextToken());
+	            int out = Integer.parseInt(st.nextToken());
+	            
+	            for(int t = in; t < out; t++) arr[t]++;
+	        }
+	        
+	        for(int fee : arr) {
+	            if(fee == 0) continue;
+	            totalFee += fee * fees[fee - 1];
+	            System.out.println(fee + " x " + fees[fee - 1] + " : " + totalFee);
+	        }
+	        
+	        System.out.println(totalFee);
+	    }
 }
