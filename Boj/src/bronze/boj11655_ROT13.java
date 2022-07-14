853c3ea51b728f348e2ea449a74bdad83eb8031a
yuu <22yuu@naver.com>
2022-07-14 23:54:53 +0900

    백준 ROT13

--- Boj/src/bronze/boj11655_ROT13.java
+++ Boj/src/bronze/boj11655_ROT13.java
@@ -6,14 +6,60 @@ import java.io.*;
 public class boj11655_ROT13 {
 	
 	public static String Convert(String s) {
+		StringBuilder sb = new StringBuilder();
 		
-		return "";
+		for(int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			char converted = ' ';
+			
+			if (48 <= ch && ch <= 57) {
+				// 정수일 경우
+		        sb.append(ch);
+		        continue;
+		    }
+			
+			if(ch >= 'a' && ch <= 'z') {
+				// 소문자
+				int rot = (int)ch + 13;
+				int z = (int)'z';
+				int a = (int)'a';
+				
+				if(rot > z) {
+					int diff = rot - z;
+					converted = (char)(a + (diff-1));
+					sb.append(converted);
+					continue;
+				}
+				sb.append((char)rot);
+				continue;
+				
+			} else {
+				// 대문자
+				int rot = (int)ch + 13;
+				int z = (int)'Z';
+				int a = (int)'A';
+				
+				if(rot > z) {
+					int diff = rot - z;
+					converted = (char)(a + (diff - 1));
+					sb.append(converted);
+					continue;
+				}
+				sb.append((char)rot);
+				continue;
+			}
+		}
+		
+		return sb.append(" ").toString();
 	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] strArr = br.readLine().split(" ");
 		
+		for(String str : strArr) {
+			System.out.print(Convert(str));
+		}
 		
 		
 
