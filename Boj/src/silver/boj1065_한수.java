7332b6d90486f3d43f0e8a9e13069f9e904f94bc
yuu <22yuu@naver.com>
2022-07-25 23:18:27 +0900

    백준 시험감독

--- Boj/src/silver/boj1065_한수.java
+++ Boj/src/silver/boj1065_한수.java
@@ -5,8 +5,6 @@ import java.io.*;
 
 public class boj1065_ÇÑ¼ö {
 
-	
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
